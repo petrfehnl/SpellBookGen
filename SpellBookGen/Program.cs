2415dbae13001a979ebb2eaca49304ef0fa1dc52
realn <realn@DESKTOP-6SDPC1I>
2022-05-22 00:03:21 +0200

    Přidejte soubory projektu.

---
+++ SpellBookGen/Program.cs
@@ -0,0 +1,52 @@
+﻿using SpellBookGen;
+
+Console.WriteLine("Generátor náhodných spellů");
+
+List<Spell> seznamSpellu = new List<Spell>();
+HelperClass pomocneMetody = new HelperClass();
+
+string[] index1 = pomocneMetody.PoleZTextu("txt/spellform.txt");
+string[] index2 = pomocneMetody.PoleZTextu("txt/spelltypes.txt");
+
+while (true)
+{
+    Console.WriteLine("1. Vygenerovat nový spell a přidat do Spellbooku");
+    Console.WriteLine("2. Zobrazit Spellbook");
+    Console.WriteLine("3. Smazat poslední záznam ve Spellbooku");
+    Console.WriteLine("4. Smazat celý SpellBook");
+    Console.WriteLine("4.*. ukončit aplikaci");
+    string input = Console.ReadLine();
+
+    if (input == "1")
+    {
+        Spell spell = new Spell();
+        spell.Name = pomocneMetody.VygenerujSpeelname(index1, index2);
+        spell.ManaCost = pomocneMetody.VygenerujCislo(100);
+        seznamSpellu.Add(spell);
+        Console.Clear();
+    }
+    else if (input == "2")
+    {
+        foreach (Spell spell in seznamSpellu)
+        {
+            Console.WriteLine($"Spell: {spell.Name} " + $" Mana cost: {spell.ManaCost} ");
+        }
+    }
+    else if (input == "3")
+    {
+        int indexPosledniho = seznamSpellu.Count - 1;
+
+        Spell posledni = seznamSpellu[indexPosledniho];
+        seznamSpellu.Remove(posledni);
+        Console.Clear();
+    }
+    else if (input == "4")
+    {
+        seznamSpellu.Clear();
+        Console.Clear();
+    }
+    else if (input == "*")
+    {
+        break;
+    }
+}
---
+++ SpellBookGen/Spell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpellBookGen
+{
+    internal class Spell
+    {
+        public string Name { get; set; }
+        public int ManaCost { get; set; }
+    }
+}
